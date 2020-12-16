98e05b8ffbc4402f1461e2549ce0b5b2f235c5dd
fo <fo@bigroses.net>
2020-12-16 15:31:20 +0100

    MFC: fix ARM64 OLE Dispatch double and float parameters

--- MFCDLL/dispimpl_supporting.h
+++ MFCDLL/dispimpl_supporting.h
@@ -9,18 +9,25 @@ typedef struct _ARM64_PARAMS
 	double DoubleValues[8];
 } ARM64_PARAMS, UNSUPPORTEDPLAT_PARAMS, * PARM64_PARAMS, * PUNSUPPORTEDPLAT_PARAMS;
 
-inline BOOL UnsupportedplatParamsReset(UNSUPPORTEDPLAT_PARAMS * p)
+
+inline void UnsupportedplatParamsReset(UNSUPPORTEDPLAT_PARAMS * p)
 {
-	return TRUE;
+	p->IntsUsed = 0;
+	p->DoublesUsed = 0;
 };
 
 inline BOOL UnsupportedplatParamsAddFloat(UNSUPPORTEDPLAT_PARAMS* p, FLOAT f)
 {
+	float * pf = (float*) &(p->DoubleValues[p->DoublesUsed]);
+	*pf = f;
+	p->DoublesUsed++;
 	return TRUE;
 };
 
 inline BOOL UnsupportedplatParamsAddDouble(UNSUPPORTEDPLAT_PARAMS* p, DOUBLE f)
 {
+	p->DoubleValues[p->DoublesUsed] = f;
+	p->DoublesUsed++;
 	return TRUE;
 };
 
